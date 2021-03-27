91328e391598a36a61f8b6afe5dca3efe57b990c
blakereeves19 <blake.reeves19@gmail.com>
2021-03-27 10:11:15 -0500

    created text component and created styles files for components

--- src/features/restaurants/screens/restaurants.screen.jsx
+++ src/features/restaurants/screens/restaurants.screen.jsx
@@ -1,24 +1,9 @@
 import React from "react";
-import { StatusBar, SafeAreaView, View } from "react-native";
 import { Searchbar } from "react-native-paper";
-import styled from "styled-components/native";
 
 import RestaurantInfoCard from "../components/restaurant-info-card.components";
 
-const SafeArea = styled(SafeAreaView)`
-  flex: 1;
-  ${StatusBar.currentHeight && `margin-top: ${StatusBar.currentHeight}px`};
-`;
-
-const SearchContainer = styled(View)`
-  padding: ${(props) => props.theme.space[3]};
-`;
-
-const ListContainer = styled(View)`
-  flex: 1;
-  padding: ${(props) => props.theme.space[3]};
-  background-color: ${(props) => props.theme.colors.bg.secondary};
-`;
+import { SafeArea, SearchContainer, ListContainer } from "./restaurants.styles";
 
 const RestaurantsScreen = () => (
   <SafeArea>
